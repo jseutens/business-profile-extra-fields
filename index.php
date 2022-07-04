490e88ccca8dbbca0c4ff475129bd39ba9cdc472
jseutens <johan@seutens.eu>
2022-07-05 00:59:26 +0200

    Update index.php

--- index.php
+++ index.php
@@ -11,11 +11,7 @@
 
 if ( ! defined( 'ABSPATH' ) )
 	exit;
-/* Add Dashicons in WordPress Front-end */
-add_action( 'wp_enqueue_scripts', 'load_dashicons_front_end' );
-function load_dashicons_front_end() {
-   wp_enqueue_style( 'dashicons' );
-}
+
 /**
  * Add extra fields to Business Profile
  *
@@ -23,9 +19,18 @@ function load_dashicons_front_end() {
  */
  add_filter( 'bpfwp_settings_page', 'prefix_add_settings', 100 ); 
 /**
- * Add settings options for a whatsapp number - add the fields
+ * Add settings options for a cellphone ,whatsapp and fax number - add the fields
  */
 function prefix_add_settings( $sap ) {
+	$sap->add_setting(
+		'bpfwp-settings',
+		'bpfwp-contact',
+		'text',
+		array(
+			'id'     => 'cellphone',
+			'title'  => __( 'Cellphone', BPFWP_TEXTDOMAIN ),
+		)
+	);
 	$sap->add_setting(
 		'bpfwp-settings',
 		'bpfwp-contact',
@@ -35,16 +40,27 @@ function prefix_add_settings( $sap ) {
 			'title'  => __( 'Whatsapp', BPFWP_TEXTDOMAIN ),
 		)
 	);
+	$sap->add_setting(
+		'bpfwp-settings',
+		'bpfwp-contact',
+		'text',
+		array(
+			'id'     => 'fax',
+			'title'  => __( 'Fax', BPFWP_TEXTDOMAIN ),
+		)
+	);
 	// Repeat the above array for further fields
 	return $sap;
 }
 
 add_filter( 'bpwfp_contact_card_defaults', 'prefix_contact_card_defaults' );
 /**
- * Add the mobile number to the defaults - true to show in widget
+ * Add the cellphone , whatsapp and fax number to the defaults - true to show in widget
  */
 function prefix_contact_card_defaults( $defaults ) {
+	$defaults['show_cellphone'] = false;
 	$defaults['show_whatsapp'] = false;
+	$defaults['show_fax'] = false;
 	return $defaults;
 }
 
@@ -55,6 +71,19 @@ add_filter( 'bpwfwp_component_callbacks', 'prefix_component_callbacks' );
 function prefix_component_callbacks( $callbacks ) {
 	global $bpfwp_controller;
     
+    if ( $bpfwp_controller->settings->get_setting( 'cellphone' ) ) {
+		$callbacks['cellphone'] = 'prefix_print_cellphone';
+		$new_callbacks = array();
+		foreach( $callbacks as $key => $val ) {
+			$new_callbacks[$key] = $val;
+			// When you find the element you want to place it after,
+			// slot it in. This positions the new field where you want it - here it is being placed after the phone field
+			if ( $key == 'phone' ) {
+				$new_callbacks['cellphone'] = 'prefix_print_cellphone';
+			}
+			$callbacks = $new_callbacks;
+	    }
+    }
     if ( $bpfwp_controller->settings->get_setting( 'whatsapp' ) ) {
 		$callbacks['whatsapp'] = 'prefix_print_whatsapp';
 		$new_callbacks = array();
@@ -62,8 +91,21 @@ function prefix_component_callbacks( $callbacks ) {
 			$new_callbacks[$key] = $val;
 			// When you find the element you want to place it after,
 			// slot it in. This positions the new field where you want it - here it is being placed after the phone field
-			if ( $key == 'phone' ) {
+			if ( $key == 'cellphone' ) {
 				$new_callbacks['whatsapp'] = 'prefix_print_whatsapp';
+			}
+			$callbacks = $new_callbacks;
+	    }
+    }
+    if ( $bpfwp_controller->settings->get_setting( 'fax' ) ) {
+		$callbacks['fax'] = 'prefix_print_fax';
+		$new_callbacks = array();
+		foreach( $callbacks as $key => $val ) {
+			$new_callbacks[$key] = $val;
+			// When you find the element you want to place it after,
+			// slot it in. This positions the new field where you want it - here it is being placed after the phone field
+			if ( $key == 'whatsapp' ) {
+				$new_callbacks['fax'] = 'prefix_print_fax';
 			}
 			$callbacks = $new_callbacks;
 	    }
@@ -72,10 +114,30 @@ function prefix_component_callbacks( $callbacks ) {
 }
 
 /**
+ * The callback - Print the cellphone number
+ * 
+ */
+function prefix_print_cellphone() {
+	// This is the mark up - is the same format as the original but has the number in a clickable link.
+	global $bpfwp_controller;
+	if ( $bpfwp_controller->display_settings['show_cellphone'] ) : ?>
+
+	<div class="bp-cellphone" itemprop="telephone">
+	<a href="tel:<?php echo (str_replace(' ','',$bpfwp_controller->settings->get_setting('cellphone') )); ?>"><?php echo $bpfwp_controller->settings->get_setting( 'cellphone' ); ?></a>
+	</div>
+
+	<?php else : ?>
+	<meta itemprop="telephone" content="<?php echo esc_attr( $bpfwp_controller->settings->get_setting( 'cellphone' ) ); ?>">
+
+
+	<?php endif;
+    }
+
+/**
  * The callback - Print the whatsapp number
  * 
  */
-function prefix_print_whatsapp() {
+    function prefix_print_whatsapp() {
 	// This is the mark up - is the same format as the original but has the number in a clickable link.
 	global $bpfwp_controller;
 	if ( $bpfwp_controller->display_settings['show_whatsapp'] ) : ?>
@@ -89,4 +151,22 @@ function prefix_print_whatsapp() {
 
 	<?php endif;
     }
-    
+
+/**
+ * The callback - Print the fax number
+ * 
+ */
+function prefix_print_fax() {
+	// This is the mark up - is the same format as the original but has the number in a clickable link.
+	global $bpfwp_controller;
+	if ( $bpfwp_controller->display_settings['show_fax'] ) : ?>
+
+	<div class="bp-fax" itemprop="telephone">
+	<a href="tel:<?php echo (str_replace(' ','',$bpfwp_controller->settings->get_setting('fax') )); ?>"><?php echo $bpfwp_controller->settings->get_setting( 'fax' ); ?></a>
+	</div>
+
+	<?php else : ?>
+	<meta itemprop="telephone" content="<?php echo esc_attr( $bpfwp_controller->settings->get_setting( 'fax' ) ); ?>">
+
+	<?php endif;
+    }
