b19c903d1c8be976f2bd95a72dec7921efbe40d0
jseutens <johan@seutens.eu>
2022-07-04 22:39:05 +0200

    Update index.php

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ if ( ! defined( 'ABSPATH' ) )
  */
  add_filter( 'bpfwp_settings_page', 'prefix_add_settings', 100 ); 
 /**
- * Add settings options for a mobile number - add the fields
+ * Add settings options for a whatsapp number - add the fields
  */
 function prefix_add_settings( $sap ) {
 	$sap->add_setting(
@@ -26,8 +26,8 @@ function prefix_add_settings( $sap ) {
 		'bpfwp-contact',
 		'text',
 		array(
-			'id'     => 'mobile',
-			'title'  => __( 'Mobile', BPFWP_TEXTDOMAIN ),
+			'id'     => 'whatsapp',
+			'title'  => __( 'Whatsapp', BPFWP_TEXTDOMAIN ),
 		)
 	);
 	// Repeat the above array for further fields
@@ -39,7 +39,7 @@ add_filter( 'bpwfp_contact_card_defaults', 'prefix_contact_card_defaults' );
  * Add the mobile number to the defaults - true to show in widget
  */
 function prefix_contact_card_defaults( $defaults ) {
-	$defaults['show_mobile'] = true;
+	$defaults['show_whatsapp'] = false;
 	return $defaults;
 }
 
@@ -50,15 +50,15 @@ add_filter( 'bpwfwp_component_callbacks', 'prefix_component_callbacks' );
 function prefix_component_callbacks( $callbacks ) {
 	global $bpfwp_controller;
     
-    if ( $bpfwp_controller->settings->get_setting( 'mobile' ) ) {
-		$callbacks['mobile'] = 'prefix_print_mobile';
+    if ( $bpfwp_controller->settings->get_setting( 'whatsapp' ) ) {
+		$callbacks['whatsapp'] = 'prefix_print_whatsapp';
 		$new_callbacks = array();
 		foreach( $callbacks as $key => $val ) {
 			$new_callbacks[$key] = $val;
 			// When you find the element you want to place it after,
 			// slot it in. This positions the new field where you want it - here it is being placed after the phone field
 			if ( $key == 'phone' ) {
-				$new_callbacks['mobile'] = 'prefix_print_mobile';
+				$new_callbacks['whatsapp'] = 'prefix_print_whatsapp';
 			}
 			$callbacks = $new_callbacks;
 	    }
@@ -67,20 +67,20 @@ function prefix_component_callbacks( $callbacks ) {
 }
 
 /**
- * The callback - Print the mobile number
+ * The callback - Print the whatsapp number
  * 
  */
-function prefix_print_mobile() {
+function prefix_print_whatsapp() {
 	// This is the mark up - is the same format as the original but has the number in a clickable link.
 	global $bpfwp_controller;
-	if ( $bpfwp_controller->display_settings['show_mobile'] ) : ?>
+	if ( $bpfwp_controller->display_settings['show_whatsapp'] ) : ?>
 
-	<div class="bp-mobile" itemprop="telephone">
-	<a href="tel:<?php echo (str_replace(' ','',$bpfwp_controller->settings->get_setting('mobile') )); ?>"><?php echo $bpfwp_controller->settings->get_setting( 'mobile' ); ?></a>
+	<div class="bp-whatsapp" itemprop="telephone">
+	<a href="tel:<?php echo (str_replace(' ','',$bpfwp_controller->settings->get_setting('whatsapp') )); ?>"><?php echo $bpfwp_controller->settings->get_setting( 'whatsapp' ); ?></a>
 	</div>
 
 	<?php else : ?>
-	<meta itemprop="telephone" content="<?php echo esc_attr( $bpfwp_controller->settings->get_setting( 'mobile' ) ); ?>">
+	<meta itemprop="telephone" content="<?php echo esc_attr( $bpfwp_controller->settings->get_setting( 'whatsapp' ) ); ?>">
 
 	<?php endif;
     }
@@ -95,8 +95,8 @@ function prefix_added_styles() {
 		plugins_url() . '/business-profile/assets/css/contact-card.css'
 	);
         $custom_css = '
-		.bp-mobile:before {
-				content: "\f10b";
+		.bp-whatsapp:before {
+				content: "fa-whatsapp";
 				font-family: "fontawesome";
 			}';
         wp_add_inline_style( 'bpfwp-default', $custom_css );
